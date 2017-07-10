17881d30182ee0d1384c4e1cd5bf95f5b4efdc6b
Lee Mulvey <lmulvey@me.com>
2017-07-10 03:28:35 +0000

    Started ./lib/earl.js to handle all earl requests to keep things cruddy.

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var http = require('http');
+var http = require('http'),
+    earl = require('./lib/earl.js');
 
 function handle_incoming_req(req, res) {
-  if(req.url == "/") req.url = "ROOT REQUEST";
-  console.log("INCOMING REQUEST: " + req.method + " from URL: " + req.url);
-  res.writeHead(200, { "Content-Type" : "application/json" });
-  res.end(JSON.stringify( { error: null }) + "\n");
+  /* Carve our routes out */
+  if(req.url == "/") {
+    req.url = "ROOT REQUEST";
+    console.log("INCOMING REQUEST: " + req.method + " from URL: " + req.url);
+    res.writeHead(200, { "Content-Type" : "application/json" });
+    res.end(JSON.stringify( { error: null }) + "\n");
+  }
+
+  if(req.url == "new") {
+    /* Create new URL forwarder */
+
+    /* Parse URL, confirm that it is indeed a valid URL.
+        Create a new EARL, output the view_created w/
+        new URL information */
+    console.log("Created new EARL. http://tinyearl.com/ " + earl.generateEarlID());
+    res.writeHead(200, { "Content-Type" : "application/json" });
+    res.end(JSON.stringify( { error: null }) + "\n");
+ }
+
 }
 
 var s = http.createServer(handle_incoming_req);
